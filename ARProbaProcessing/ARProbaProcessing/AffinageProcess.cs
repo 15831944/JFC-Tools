f1bc4fbf035c8d005fc1c56f0ffc8691a8aecfb3
KT\Frederic.Legeay <frederic.legeay@kantarmedia.com>
2020-10-06 14:26:39 +0200

    Ajout des méthodes

--- ARProbaProcessing/ARProbaProcessing/AffinageProcess.cs
+++ ARProbaProcessing/ARProbaProcessing/AffinageProcess.cs
@@ -1,12 +1,150 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace ARProbaProcessing
+﻿namespace ARProbaProcessing
 {
     public class AffinageProcess
     {
+        public void Run()
+        {
+            lecpanel();
+            segpanel();
+            ecrpan1j();
+            regr5jp2();
+            fushab09();
+            chab1qhp();
+            crenonin();
 
+            ecrsegpa();
+            CALCREGR();
+            calcnivo();
+            caud1qhp();
+
+            caudtotp();
+
+            sav1qhpa();
+            sav1qhps();
+            sav1qhpd();
+
+            cgrp75br();
+            cont75br();
+
+            cnzuptse();
+            cnzuptsa();
+            cnzuptdi();
+
+            attribp2();
+
+            transp08();
+            crecib08();
+            penetr();
+            asympt();
+        }
+
+        private void lecpanel()
+        {
+
+        }
+
+        private void segpanel()
+        {
+
+        }
+        private void ecrpan1j()
+        {
+
+        }
+        private void regr5jp2()
+        {
+
+        }
+        private void fushab09()
+        {
+
+        }
+        private void chab1qhp()
+        {
+
+        }
+        private void crenonin()
+        {
+
+        }
+
+        private void ecrsegpa()
+        {
+
+        }
+        private void CALCREGR()
+        {
+
+        }
+        private void calcnivo()
+        {
+
+        }
+        private void caud1qhp()
+        {
+
+        }
+
+        private void caudtotp()
+        {
+
+        }
+
+        private void sav1qhpa()
+        {
+
+        }
+        private void sav1qhps()
+        {
+
+        }
+        private void sav1qhpd()
+        {
+
+        }
+
+        private void cgrp75br()
+        {
+
+        }
+        private void cont75br()
+        {
+
+        }
+
+        private void cnzuptse()
+        {
+
+        }
+        private void cnzuptsa()
+        {
+
+        }
+        private void cnzuptdi()
+        {
+
+        }
+
+        private void attribp2()
+        {
+
+        }
+
+        private void transp08()
+        {
+
+        }
+        private void crecib08()
+        {
+
+        }
+        private void penetr()
+        {
+
+        }
+        private void asympt()
+        {
+
+        }
     }
 }
