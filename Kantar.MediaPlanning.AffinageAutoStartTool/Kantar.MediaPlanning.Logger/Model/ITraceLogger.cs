844ed237745c38039f8e17aa8aad58b610952a76
KT\Frederic.Legeay <frederic.legeay@kantarmedia.com>
2021-02-01 18:18:41 +0100

    Kantar.MediaPlanning.AffinageAutoStartTool

---
+++ Kantar.MediaPlanning.AffinageAutoStartTool/Kantar.MediaPlanning.Logger/Model/ITraceLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kantar.MediaPlanning.Logger.Model
+{
+    interface ITraceLogger
+    {
+        void SetupTrace();
+        void SetupTrace(string defaultTraceSwitchName);
+        TraceLevel Level { get; }
+        TraceSwitch DefaultTraceSwitch { get; }
+        void WriteLineIf(TraceLevel oneLevel, string oneMessage);
+        void WriteLine(string oneMessage);
+    }
+}
